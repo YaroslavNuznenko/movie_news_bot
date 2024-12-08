f48cf72e7670a6f302c3520c0824ccd1b048965b
Yaroslav Nuzhnenko <nuznenko@gmail.com>
2024-12-08 17:24:02 +0200

    Init movie news bot

--- location/__init__.py
+++
@@ -1,2 +0,0 @@
-from .location_api import *  # noqa
-from .nominatim_api import *  # noqa
--- main.py
+++ main.py
@@ -1,35 +1,32 @@
-import asyncio
 import logging
+import asyncio
 import os
 import sys
 
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
+from aiogram import Bot
 from dotenv import load_dotenv
 
-from chat import settings_router
-from location import NominatimAPI
-from weather_forecast import WeatherAPI
+import parsers.variety as variety
+
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+
+
+
+
+bot = Bot(token=BOT_TOKEN)
 
 
 async def main() -> None:
-    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    location_api = NominatimAPI()
-    weather_forecast_api = WeatherAPI(WEATHER_API_KEY)
-
-    dp = Dispatcher(location_api=location_api, weather_forecast_api=weather_forecast_api)
-    dp.include_router(settings_router)
-
-    await dp.start_polling(bot)
-
+   await variety.fetch_articles(bot)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
+
+
+
+
